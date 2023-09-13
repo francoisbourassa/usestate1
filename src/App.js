b1e4d8de9f752573349e8ace21974eea37484dd4
francois bourassa <you@example.com>
2023-09-13 15:31:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Utilisation du hook useState pour gérer l'état du compteur
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Compteur : {count}</p>
+        <button onClick={() => setCount(count + 1)}>
+          Augmenter
+        </button>
+        <button onClick={() => setCount(count - 1)}>
+          Diminuer
+        </button>
+        <button onClick={() => setCount(0)}>
+          Réinitialiser
+        </button>
       </header>
     </div>
   );
